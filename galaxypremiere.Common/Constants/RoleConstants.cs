115ff9f7cf45b932a2004f15b75e440181e83ae4
Alimohammad Eghbaldar <ali.eghbaldar2@gmail.com>
2023-09-18 09:19:39 +0330

    data-seeding for ROLE table

---
+++ galaxypremiere.Common/Constants/RoleConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace galaxypremiere.Common.Constants
+{
+    public abstract class RoleConstants
+    {
+        public const string King = "King";
+        public const string SuperAdmin = "SuperAdmin";
+        public const string Admin = "Admin";
+        public const string Client = "SuperAdmin";
+        public const string User = "User";
+    }
+}
--- galaxypremiere.Persistence/Contexts/DataBaseContext.cs
+++ galaxypremiere.Persistence/Contexts/DataBaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using galaxypremiere.Application.Interfaces.Contexts;
+using galaxypremiere.Common.Constants;
 using galaxypremiere.Domain.Entities.Users;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -9,14 +10,28 @@ using System.Threading.Tasks;
 
 namespace galaxypremiere.Persistence.Context
 {
-    public class DataBaseContext:DbContext,IDataBaseContext
+    public class DataBaseContext : DbContext, IDataBaseContext
     {
-        public DataBaseContext(DbContextOptions option) : base (option)
+        public DataBaseContext(DbContextOptions option) : base(option)
         {
 
         }
+
+        // Tables
         public DbSet<Users> Users { get; set; } // Users Table
         public DbSet<Roles> Roles { get; set; } // Roles Table
         public DbSet<UsersInRoles> UsersInRoles { get; set; } // UsersInRoles Table
+        // End
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Data-Seeding
+            modelBuilder.Entity<Roles>().HasData(new Roles { Id = 1, Name = nameof(RoleConstants.King) });
+            modelBuilder.Entity<Roles>().HasData(new Roles { Id = 2, Name = nameof(RoleConstants.SuperAdmin) });
+            modelBuilder.Entity<Roles>().HasData(new Roles { Id = 3, Name = nameof(RoleConstants.Admin) });
+            modelBuilder.Entity<Roles>().HasData(new Roles { Id = 4, Name = nameof(RoleConstants.Client) });
+            modelBuilder.Entity<Roles>().HasData(new Roles { Id = 5, Name = nameof(RoleConstants.User) });
+            // End
+        }
     }
 }
