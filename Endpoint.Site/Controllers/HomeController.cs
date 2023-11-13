d7e5ba23e1f688a91589947b4ae1f05d72576e51
Alimohammad Eghbaldar <ali.eghbaldar2@gmail.com>
2023-11-13 16:05:48 +0330

    before adding PROVIDER property to USER entity

--- Endpoint.Site/Controllers/HomeController.cs
+++ Endpoint.Site/Controllers/HomeController.cs
@@ -7,11 +7,25 @@ using System.Diagnostics;
 using System.Security.Claims;
 using AspNet.Security.OAuth.Instagram;
 using Microsoft.AspNetCore.Http;
+using galaxypremiere.Infrastructure.Filters;
+using galaxypremiere.Application.Services.Users.Queries.AuthUsers;
+using galaxypremiere.Application.Interfaces.FacadePattern;
+using galaxypremiere.Application.Services.UserLoginLog.Commands.PostUserLoginLog;
 
 namespace Endpoint.Site.Controllers
 {
+    //[ModelStateAttribute]
     public class HomeController : Controller
     {
+        private readonly IUserFacade _userFacade;
+        private readonly IUserLoginLogFacade _userLoginLogFacade;
+        public HomeController(
+            IUserFacade userFacade,
+            IUserLoginLogFacade userLoginLogFacade)
+        {
+            _userFacade = userFacade;
+            _userLoginLogFacade = userLoginLogFacade;
+        }
         public IActionResult Index()
         {
             return View();
@@ -20,26 +34,58 @@ namespace Endpoint.Site.Controllers
         {
             return View();
         }
-        public async Task Login(string returnUrl)
+        //public async Task Login(string returnUrl)
+        //{
+        //    await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme, new AuthenticationProperties()
+        //    {
+        //        RedirectUri = Url.Action("GoogleResponse")
+        //    });
+        //}
+        //public async Task<IActionResult> GoogleResponse(string returnUrl)
+        //{
+        //    var loginInfo = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        //    string email = loginInfo.Principal.FindFirst(ClaimTypes.Email).Value;
+        //    string firstname = loginInfo.Principal.FindFirst(ClaimTypes.GivenName)?.Value?? null;
+        //    string lastname = loginInfo.Principal.FindFirst(ClaimTypes.Surname)?.Value?? null;
+
+        //    if(Url.IsLocalUrl(returnUrl)) { 
+        //        //
+        //    }
+
+        //    return View();
+        //}
+        [HttpPost]
+        public IActionResult Login(RequestAuthLoginUsersServiceDto req)
         {
-            await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme, new AuthenticationProperties()
+            var login = _userFacade.AuthLoginUsersService.Execute(req);
+            if (login != null)
             {
-                RedirectUri = Url.Action("GoogleResponse")
-            });
-        }
-        public async Task<IActionResult> GoogleResponse(string returnUrl)
-        {
-            var loginInfo = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                if (login.IsSuccess)
+                {
+                    var claims = new List<Claim>() {
+                        new Claim(ClaimTypes.NameIdentifier,login.Data.IdUser.ToString()),
+                        new Claim(ClaimTypes.Email,req.Email),
+                        new Claim(ClaimTypes.Name,login.Data.Fullname),
+                        new Claim(ClaimTypes.Role,login.Data.Role),
+                    };
 
-            string email = loginInfo.Principal.FindFirst(ClaimTypes.Email).Value;
-            string firstname = loginInfo.Principal.FindFirst(ClaimTypes.GivenName)?.Value?? null;
-            string lastname = loginInfo.Principal.FindFirst(ClaimTypes.Surname)?.Value?? null;
-
-            if(Url.IsLocalUrl(returnUrl)) { 
-                //
+                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var principal = new ClaimsPrincipal(identity);
+                    var propertise = new AuthenticationProperties()
+                    {
+                        IsPersistent = true,
+                        ExpiresUtc = DateTime.Now.AddYears(1),
+                    };
+                    _userLoginLogFacade.PostUserLoginLogService.Execute(new RequestPostUserLoginLogServiceDto
+                    {
+                        UsersId = login.Data.IdUser,
+                        IP = Request.HttpContext.Connection.RemoteIpAddress.ToString(),
+                    });
+                    HttpContext.SignInAsync(principal, propertise);
+                }
             }
-
-            return View();
+            return Json(login);
         }
 
     }
