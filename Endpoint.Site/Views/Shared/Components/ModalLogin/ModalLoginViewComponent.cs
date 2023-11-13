6ded1231b29c69ca611a8713c5e456b88443141c
Alimohammad Eghbaldar <ali.eghbaldar2@gmail.com>
2023-11-13 14:10:30 +0330

    use component for modal login

---
+++ Endpoint.Site/Views/Shared/Components/ModalLogin/ModalLoginViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Endpoint.Site.Views.Shared.Components.Login
+{
+    public class ModalLoginViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("Index");
+        }
+    }
+}
