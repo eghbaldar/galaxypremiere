f82628ebdce329be9e3b926b88dc4cdb101d0123
Alimohammad Eghbaldar <ali.eghbaldar2@gmail.com>
2023-11-18 16:01:25 +0330

    add new user template

---
+++ Endpoint.Site/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Endpoint.Site.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Me()
+        {
+            return View();
+        }
+    }
+}
---
+++ Endpoint.Site/Views/Shared/Components/TopRightUserPanel/TopRightUserPanelViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Endpoint.Site.Utilities;
+using galaxypremiere.Application.Interfaces.FacadePattern;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Endpoint.Site.Views.Shared.Components;
+
+public class TopRightUserPanelViewComponent : ViewComponent
+{
+    public static String fullname;
+    public IViewComponentResult Invoke()
+    {
+        fullname = ClaimUtility.GetUserFullname(User as ClaimsPrincipal);
+        return View("Index");
+    }
+}
