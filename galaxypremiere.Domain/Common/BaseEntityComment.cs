9fa78198233e7068fc29ed051cccba08b16587fe
Alimohammad Eghbaldar <ali.eghbaldar2@gmail.com>
2024-02-18 10:01:15 +0330

    before create photo comment

---
+++ galaxypremiere.Domain/Common/BaseEntityComment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace galaxypremiere.Domain.Common
+{
+    public class BaseEntityComment:BaseEntityGuid
+    {
+        public Guid? Parent { get; set; } = null; // x=null=> main comment | x!=null => subComment
+        public string Comment { get; set; }
+    }
+}
---
+++ galaxypremiere.Domain/Entities/Users/UsersPhotoComments.cs
@@ -0,0 +1,17 @@
+﻿using galaxypremiere.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace galaxypremiere.Domain.Entities.Users
+{
+    public class UsersPhotoComments : BaseEntityComment
+    {
+        public long UsersId { get; set; }
+        public virtual Users Users { get; set; }
+        public long UsersPhotosId { get; set; }
+        public virtual UsersPhotos UsersPhotos { get; set; }
+    }
+}
