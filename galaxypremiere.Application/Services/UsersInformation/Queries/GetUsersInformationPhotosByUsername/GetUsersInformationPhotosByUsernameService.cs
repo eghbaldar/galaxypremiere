9bcaf95b905c98be484a1ac040a3a5438d7c7020
Alimohammad Eghbaldar <ali.eghbaldar2@gmail.com>
2024-03-01 19:54:12 +0330

    before put inner style into separate file

---
+++ galaxypremiere.Application/Services/UsersInformation/Queries/GetUsersInformationPhotosByUsername/GetUsersInformationPhotosByUsernameService.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using galaxypremiere.Application.Interfaces.Contexts;
+using galaxypremiere.Common.Constants;
+
+namespace galaxypremiere.Application.Services.UsersInformation.Queries.GetUsersInformationPhotosByUsername
+{
+    public class GetUsersInformationPhotosByUsernameService : IGetUsersInformationPhotosByUsernameService
+    {
+        private readonly IDataBaseContext _context;
+        private readonly IMapper _mapper;
+        public GetUsersInformationPhotosByUsernameService(IDataBaseContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public ResultGetUsersInformationPhotosByUsernameServiceDto Execute(RequestGetUsersInformationPhotosByUsernameServiceDto req)
+        {
+            if (req == null) return null;
+
+            var groupedComments = _context.UsersPhotoComments
+                    .GroupBy(p => p.UsersPhotosId, p => p.Comment, (key, g) => new { PhotoId = key, Count = g.Count() });
+            var groupedLikes = _context.Likes
+                     .Where(l => l.DeleteTime == null && l.Section == SectionsConstants.UserPhotos)
+                     .GroupBy(l => new { l.SectionId, l.Section })
+                     .Select(g => new { SectionId = g.Key.SectionId, Section = g.Key.Section, Count = g.Count() });
+
+            // x==0  meants it is a guest and x!=0 it is a user and her/his "userId" has just been returned.
+            var currentUserLikeUnlike = _context.Likes.Where(l => l.UsersId == req.UserId && l.DeleteTime == null).AsQueryable();
+
+            // here SectionId is equal too PhotoId
+            var photos =
+                (
+                from p in _context.UsersPhotos
+                join albums in _context.UsersAlbums on p.UsersAlbumsId equals albums.Id into GroupAlbums
+                from albums in GroupAlbums.DefaultIfEmpty()
+                join info in _context.UsersInformation on albums.UsersId equals info.UsersId into GroupUser
+                from info in GroupUser.DefaultIfEmpty()
+                where (info.Username == req.Username)
+                select new
+                {
+                    Photos = p,
+                }
+                )
+                .OrderBy(p => p.Photos.InsertDate)
+                .Select(p => new GetUsersInformationPhotosByUsernameServiceDto
+                {
+                    Filename = p.Photos.Filename,
+                    Id = p.Photos.Id,
+                    Title = p.Photos.Title,
+                    Detail = p.Photos.Detail,
+                    VisitorCounter = p.Photos.VisitorCounter + 1,
+                    CountComments = groupedComments.Where(gc => gc.PhotoId == p.Photos.Id).Select(gc => gc.Count).First().ToString(),
+                    CountLikes = groupedLikes.Where(gl => gl.SectionId == p.Photos.Id).Select(gl => gl.Count).First().ToString(),
+                    Like = currentUserLikeUnlike.Where(x => x.SectionId == p.Photos.Id).Any(),
+                })
+                .Take(6)
+                .ToList();
+
+            if (photos != null)
+            {
+                return new ResultGetUsersInformationPhotosByUsernameServiceDto
+                {
+                    resultGetUsersInformationPhotosByUsernameServiceDto = photos,
+                };
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- galaxypremiere.Application/Services/UsersInformation/Queries/GetUsersInformationPhotosByUsername/IGetUsersInformationPhotosByUsernameService.cs
+++ galaxypremiere.Application/Services/UsersInformation/Queries/GetUsersInformationPhotosByUsername/IGetUsersInformationPhotosByUsernameService.cs
@@ -1,7 +1,4 @@
-﻿using AutoMapper;
-using galaxypremiere.Application.Interfaces.Contexts;
-using galaxypremiere.Common.Constants;
-using galaxypremiere.Common.DTOs;
+﻿using galaxypremiere.Common.DTOs;
 using galaxypremiere.Domain.Entities.Users;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -17,70 +14,5 @@ namespace galaxypremiere.Application.Services.UsersInformation.Queries.GetUsersI
     public interface IGetUsersInformationPhotosByUsernameService
     {
         ResultGetUsersInformationPhotosByUsernameServiceDto Execute(RequestGetUsersInformationPhotosByUsernameServiceDto req);
-    }
-    public class GetUsersInformationPhotosByUsernameService : IGetUsersInformationPhotosByUsernameService
-    {
-        private readonly IDataBaseContext _context;
-        private readonly IMapper _mapper;
-        public GetUsersInformationPhotosByUsernameService(IDataBaseContext context, IMapper mapper)
-        {
-            _context = context;
-            _mapper = mapper;
-        }
-        public ResultGetUsersInformationPhotosByUsernameServiceDto Execute(RequestGetUsersInformationPhotosByUsernameServiceDto req)
-        {
-            if (req == null) return null;
-
-            var groupedComments = _context.UsersPhotoComments
-                    .GroupBy(p => p.UsersPhotosId, p => p.Comment, (key, g) => new { PhotoId = key, Count = g.Count() });
-            var groupedLikes = _context.Likes
-                     .Where(l => l.DeleteTime == null && l.Section == SectionsConstants.UserPhotos)
-                     .GroupBy(l => new { l.SectionId, l.Section })
-                     .Select(g => new { SectionId = g.Key.SectionId, Section = g.Key.Section, Count = g.Count() });
-
-            // x==0  meants it is a guest and x!=0 it is a user ant its "userId" has just been returned.
-            var currentUserLikeUnlike = _context.Likes.Where(l => l.UsersId == req.UserId && l.DeleteTime == null).AsQueryable();
-            
-            // here SectionId is equal too PhotoId
-            var photos =
-                (
-                from p in _context.UsersPhotos
-                join albums in _context.UsersAlbums on p.UsersAlbumsId equals albums.Id into GroupAlbums
-                from albums in GroupAlbums.DefaultIfEmpty()
-                join info in _context.UsersInformation on albums.UsersId equals info.UsersId into GroupUser
-                from info in GroupUser.DefaultIfEmpty()
-                where (info.Username == req.Username)
-                select new
-                {
-                    Photos = p,
-                }
-                )
-                .OrderBy(p => p.Photos.InsertDate)
-                .Select(p => new GetUsersInformationPhotosByUsernameServiceDto
-                {
-                    Filename = p.Photos.Filename,
-                    Id = p.Photos.Id,
-                    Title = p.Photos.Title,
-                    Detail = p.Photos.Detail,
-                    VisitorCounter = p.Photos.VisitorCounter + 1,
-                    CountComments = groupedComments.Where(gc => gc.PhotoId == p.Photos.Id).Select(gc => gc.Count).First().ToString(),
-                    CountLikes = groupedLikes.Where(gl => gl.SectionId == p.Photos.Id).Select(gl=>gl.Count).First().ToString(),
-                    Like = currentUserLikeUnlike.Where(x=>x.SectionId==p.Photos.Id).Any(),
-                })
-                .Take(6)
-                .ToList();
-
-            if (photos != null)
-            {
-                return new ResultGetUsersInformationPhotosByUsernameServiceDto
-                {
-                    resultGetUsersInformationPhotosByUsernameServiceDto = photos,
-                };
-            }
-            else
-            {
-                return null;
-            }
-        }
     }
 }
