c080a0b23506eaf9c1865ba9c9b21efba797da24
Alimohammad Eghbaldar <ali.eghbaldar2@gmail.com>
2024-03-18 16:24:40 +0330

    step of showing post photos is done beautifully

--- Endpoint.Site/wwwroot/lib/kingFuncs/lightbox/lightbox.js
+++ Endpoint.Site/wwwroot/lib/kingFuncs/lightbox/lightbox.js
@@ -1,4 +1,5 @@
-﻿const KingGalleryImages = document.querySelectorAll('.gallery-image');
+﻿// Developed by: ALIMOHAMMAD EGHBALDAR // www.eghbaldar.ir //
+const KingGalleryImages = document.querySelectorAll('.gallery-image');
 const Kinglightbox = document.getElementById('Kinglightbox');
 const KinglightboxImage = document.getElementById('Kinglightbox-image');
 const KingcloseBtn = document.querySelector('.KinglightboxClose');
@@ -6,7 +7,7 @@ const KingprevBtn = document.getElementById('KinglightboxPrev');
 const KingnextBtn = document.getElementById('KinglightboxNext');
 
 var srcValues = [];
-var currentIndex = 0;
+var KingLightBocCurrentIndex = 0;
 var holderRootPath;
 
 function setUniquePostPhotosId(id_photo, rootPath, id_post) {
@@ -26,7 +27,16 @@ function setUniquePostPhotosId(id_photo, rootPath, id_post) {
         };
         srcValues.push(imageInfo);        
     });
-    currentIndex = findIndexBySrc(srcValues,firstSelectedPhoto);
+    KingLightBocCurrentIndex = findIndexBySrc(srcValues, firstSelectedPhoto);
+    // if there is only one element in array, the NEXT and PREVIOUS buttons will be invisibled!
+    if (srcValues.length == 1) {
+        KingnextBtn.style.display = "none";
+        KingprevBtn.style.display = "none";
+    }
+    else {
+        KingnextBtn.style.display = "block";
+        KingprevBtn.style.display = "block";
+    }
 }
 // Function to find the index based on src value
 function findIndexBySrc(arr,src) {
@@ -45,13 +55,13 @@ function KingLightboxShowImageSeeker(filename) {
 }
 // Previous image
 KingprevBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + srcValues.length) % srcValues.length;
-    KingLightboxShowImageSeeker(srcValues[currentIndex].src);
+    KingLightBocCurrentIndex = (KingLightBocCurrentIndex - 1 + srcValues.length) % srcValues.length;
+    KingLightboxShowImageSeeker(srcValues[KingLightBocCurrentIndex].src);
 });
 // Next image
 KingnextBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % srcValues.length;
-    KingLightboxShowImageSeeker(srcValues[currentIndex].src);
+    KingLightBocCurrentIndex = (KingLightBocCurrentIndex + 1) % srcValues.length;
+    KingLightboxShowImageSeeker(srcValues[KingLightBocCurrentIndex].src);
 });
 // Close when is clicked on background
 Kinglightbox.addEventListener('click', (event) => {
@@ -63,4 +73,20 @@ Kinglightbox.addEventListener('click', (event) => {
 // Close with btnClose
 KingcloseBtn.addEventListener('click', () => {
     Kinglightbox.style.display = 'none';
+});
+// Event listener for the left and right arrow keys
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowLeft') {
+        // Previous image
+        currentIndex = (currentIndex - 1 + srcValues.length) % srcValues.length;
+        KingLightboxShowImageSeeker(srcValues[currentIndex].src);
+    } else if (event.key === 'ArrowRight') {
+        // Next image
+        currentIndex = (currentIndex + 1) % srcValues.length;
+        KingLightboxShowImageSeeker(srcValues[currentIndex].src);
+    }
+    else if (event.key === 'Escape') {
+        // Close modal
+        Kinglightbox.style.display = 'none';
+    }
 });
